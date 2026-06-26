2bab0842047526ad0679855d5e6df187d015a4c8
kirav <kiravillagracia@yahoo.com>
2026-06-26 21:26:03 +0800

    used public address

--- shared/src/commonMain/kotlin/com/kira/kmp/utils/Constants.kt
+++ shared/src/commonMain/kotlin/com/kira/kmp/utils/Constants.kt
@@ -1,7 +1,8 @@
 package com.kira.kmp.utils
 
 object Constants {
-    const val BASE_URL = "https://filipinorecipeapi.onrender.com/api/"
+    //const val BASE_URL = "https://filipinorecipeapi.onrender.com/api/"
+    const val BASE_URL = "http://209.97.174.255:8080/api"
 
     const val CONNECT_TIMEOUT = 60_000L
     const val READ_TIMEOUT = 60_000L
